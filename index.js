b9369666a3a67a64b219cb8b1f221604a366ecce
Mayank Sood <mayanksood100@gmail.com>
2020-10-27 13:59:29 -0400

    Used middleware to achieve Routing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const router = express.Router();
 const port = 3000;
 
 fs.readFile('./Lab3-timetable-data.json', 'utf-8', (err, jsonString) => {
@@ -14,11 +15,29 @@ fs.readFile('./Lab3-timetable-data.json', 'utf-8', (err, jsonString) => {
 
 app.use('/', express.static('static'));
 
+app.use((req,res,next)=>{
+    console.log(`${req.method} request for ${req.url}`);
+    next();
+})
 
-app.get('/api/courses', (req,res)=>{
-    console.log(`GET request for ${req.url}`);
+router.get('/', (req,res)=>{
     res.send(data);
 });
+
+router.get('/:subject', (req,res)=>{
+    let subject = data.filter(c => c.subject==(req.params.subject))
+    for(let i=0; i<subject.length; i++){
+    if(subject){
+         res.send(subject)
+    }
+    else {
+       res.status(404).send(`Subject ${subject} was not found`);
+    };
+}
+});
+
+
+app.use('/api/courses', router)
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
